5f9e36edf39d55aa1aa84389792f9c1d982ca89d
lauren.sciullo <lauren.sciullo@MU-C1-06.mullen.pvt.k12.co.us>
2020-01-13 11:52:40 -0700

    LS initial commit

--- Project1/src/Runner.java
+++ Project1/src/Runner.java
@@ -1,10 +1,20 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Runner
 	{
 
-		public static void main(String[] args)
+		public static void main(String[] args) throws FileNotFoundException
 			{
 				// TODO Auto-generated method stub
+				
+				
+			//	Scanner file = new Scanner(new File ("studentInfo.txt"));
+		//		System.out.println(file);
+				sortStudents.sortByLastName();
+				sortStudents.sortByGpa();
+				sortStudents.sortByPeriod();
 
 			}
 
---
+++ Project1/src/sortStudents.java
@@ -0,0 +1,26 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class sortStudents
+	{
+	
+		
+		public static void sortByLastName() throws FileNotFoundException
+		{
+			System.out.println("test");
+			//Scanner file = new Scanner(new File ("studentInfo.txt"));
+			//System.out.println(file);
+		}
+		
+		public static void sortByGpa()
+		{
+			
+		}
+		
+		public static void sortByPeriod()
+		{
+			
+		}
+
+	}
