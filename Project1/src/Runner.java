aa224060208d143635b95fdd6bde94f6d922d7b9
cole.nading <cole.nading@MU-C1-10.mullen.pvt.k12.co.us>
2020-01-13 11:36:39 -0700

    Intro

--- Project1/src/Runner.java
+++ Project1/src/Runner.java
@@ -4,8 +4,15 @@ public class Runner
 
 		public static void main(String[] args)
 			{
-				// TODO Auto-generated method stub
+				into();
 
+			}
+
+		private static void into()
+			{
+				System.out.println("What would you like to do?");
+				System.out.println("1) Add or delete a student \n2) Change student grades or schedule\n3) Sort Students");
+				
 			}
 
 	}
