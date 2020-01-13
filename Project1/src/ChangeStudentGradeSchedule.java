6ff1535c4d1a217eed17ecdb05949fcedca205fa
clancy.meade <clancy.meade@mu-c1-16.mullen.pvt.k12.co.us>
2020-01-13 11:54:54 -0700

    added new class for changing student grades and schedules

---
+++ Project1/src/ChangeStudentGradeSchedule.java
@@ -0,0 +1,114 @@
+
+public class ChangeStudentGradeSchedule
+	{
+		//Michael line 4
+		public static void changeGrades()
+		{
+			System.out.println("Hello Michael");
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
+		//Clancy: line 106
+		public static void switchClasses()
+		{
+			
+		}
+		
+		
+	}
