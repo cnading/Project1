05ecad439b7808602fa9ddfcef939dcfe4212697
cole.nading <cole.nading@MU-C1-10.mullen.pvt.k12.co.us>
2020-01-14 11:22:20 -0700

    Intro setup

--- Project1/src/AddOrDel.java
+++ Project1/src/AddOrDel.java
@@ -1,7 +1,34 @@
+import java.util.Scanner;
 
 public class AddOrDel
 	{
-	 public static void intro(){
-		 System.out.println("Coles stuff");
-	 }
+		static Scanner userInput = new Scanner(System.in);
+		static int choice;
+
+		public static void intro()
+			{
+				System.out.println("Would you like to...\n1) Add a student\n2) Remove a student"
+						+ "\n3) Back to Main Menu");
+				choice = userInput.nextInt();
+				if(choice==1){
+					addStudent();
+				}
+				else if(choice==2){
+					removeStudent();
+				}
+				else if(choice==3){
+					Runner.intro();
+				}
+				else{
+					intro();
+				}
+			}
+		
+		public static void addStudent(){
+			
+		}
+		
+		public static void removeStudent(){
+			
+		}
 	}
