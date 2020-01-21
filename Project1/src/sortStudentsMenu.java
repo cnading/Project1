2dc5c153870818f3ffbb42750620894c75a00979
lauren.sciullo <lauren.sciullo@MU-C1-06.mullen.pvt.k12.co.us>
2020-01-21 12:42:02 -0700

    fixed period sorter

--- Project1/src/sortStudentsMenu.java
+++ Project1/src/sortStudentsMenu.java
@@ -64,15 +64,13 @@ public class sortStudentsMenu
 		
 		public static void sortPeriod()
 		{
-			
-					 Collections.sort(Runner.roster, new sortStudentsPeriod());
+			System.out.println("Which period would you like to see? \n(1)First \n(2)Second \n(3)Third");
+			sortChoice = userInput.nextInt();
+			Collections.sort(Runner.roster, new sortStudentNames());
+			 Collections.sort(Runner.roster, new sortStudentsPeriod());
 
 					 Runner.printRoster();
-				 
-			
-					Collections.sort(Runner.roster, new sortStudentsPeriod());
-					Runner.printRoster(); 
-
+				  
 					System.out.println();
 					intro();
 		}
--- Project1/src/sortStudentsPeriod.java
+++ Project1/src/sortStudentsPeriod.java
@@ -1,16 +1,28 @@
 import java.util.Comparator;
+import java.util.Scanner;
 
 public class sortStudentsPeriod implements Comparator<Student>
 	{
+		static Scanner userInput = new Scanner(System.in);
+		static int sortChoice = 0;
 
 	@Override
 	public int compare(Student s1, Student s2)
 		{
-			
-			return s1.getFirstClass().compareTo(s2.getFirstClass()) +
-			s1.getSecondClass().compareTo(s2.getSecondClass()) +
-			 s1.getThirdClass().compareTo(s2.getThirdClass());
-			
-		}
+			if(sortStudentsMenu.sortChoice == 1)
+				{
+				return s1.getFirstClass().compareTo(s2.getFirstClass());
+				}
+			else if(sortStudentsMenu.sortChoice == 2)
+				{
+					return s1.getSecondClass().compareTo(s2.getSecondClass());
+				}
+			else
+				{
+					return  s1.getThirdClass().compareTo(s2.getThirdClass());
+				}
+	
+					
+		}	
 
 	}
