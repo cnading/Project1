375832ee85da22c3b46c140ddf91e3d28a9a8d1a
clancy.meade <clancy.meade@mu-c1-16.mullen.pvt.k12.co.us>
2020-01-21 11:44:28 -0700

    Finished the grade changing

--- Project1/src/ChangeStudentGradeSchedule.java
+++ Project1/src/ChangeStudentGradeSchedule.java
@@ -4,84 +4,145 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-
 public class ChangeStudentGradeSchedule
 	{
-		
+
 		public static void IntroForGradeAndScheduleChange()
 			{
-				
+
 				Scanner userInput = new Scanner(System.in);
 				System.out.println(" Hello, would you like to (1) change Grades or (2) Change Schedule?");
-				
-				boolean studentList = true ;
-				while(studentList) {
-				int catagory = userInput.nextInt(); 
-				if(catagory == 1)
+
+				boolean studentList = true;
+				while (studentList)
 					{
-						
-						changeGrades();
-						studentList = false;
+						int catagory = userInput.nextInt();
+						if (catagory == 1)
+							{
+
+								changeGrades();
+								studentList = false;
+							} else if (catagory == 2)
+							{
+								switchClasses();
+								studentList = false;
+							}
+
+						else
+							{
+								System.out.println("Please pick either 1 or 2.");
+
+							}
 					}
-				else if( catagory == 2)		
-					{
-						switchClasses();
-						studentList = false; 
-					}
-				
-				else 
-					{
-						System.out.println("Please pick either 1 or 2.");
-						
-					}
-				}
-			
-			} 
-		
+
+			}
+
 		public static void changeGrades()
-		{
-	Scanner userInput = new Scanner(System.in); 		
-	
-	System.out.println("Okay, here are all of the students: ");
-			
-			Runner.printRoster(); 
+			{
+				Scanner userInput = new Scanner(System.in);
 
-	System.out.println("Please enter the first name of a student to change their grades");
-	
-	String chosenStudentName = userInput.next(); 
-	
-	for(Student s: Runner.roster)
-		{ 
-			
-		}
-	
-		}
-	
-		
-		
-		
-		
-		
-		
-		
+				System.out.println("Okay, here are all of the students: ");
 
-		
+				Runner.printRoster();
+
+				
+				
+				System.out.println("Please enter the first name of a student to change their grades.");
+				
+				String firstName = userInput.next();
+
+				System.out.println("Please enter the last name of the student.");
+				
+				String lastName = userInput.next();
+				
+				String fullName = firstName + " " + lastName; 
+				
+
+				for (Student s : Runner.roster)
+					{
+						if((s.getFirstName() + " " + s.getLastName()).equals(fullName))
+							{ 
+								System.out.println("Here is the student: " + s.getFirstName() + " " + s.getLastName() + " " + s.getFirstClass() + " " + s.getFirstClassGrade() + " " + s.getSecondClass() + " " + s.getSecondClassGrade() + " " + s.getThirdClass() + " " + s.getThirdClassGrade() + " " + 0.0);
+								
+								System.out.println("Which class would you like to change the grade in?\n1) " + s.getFirstClass() + " " + s.getFirstClassGrade()  + "\n2) " + s.getSecondClass() + " " + s.getSecondClassGrade()  + "\n3) " + s.getThirdClass() + " " + s.getThirdClassGrade());
+								
+								int chosenClass = userInput.nextInt(); 
+								
+								if(chosenClass == 1) 
+									{ 
+										System.out.println("You are changing the grade in " + s.getFirstClass());
+										
+										System.out.println("What would you like the new grade to be?");
+										
+										String chosenGrade = userInput.next(); 
+										
+										s.setFirstClassGrade(chosenGrade);
+										
+										System.out.println("Okay, the grade has been changed");
+										
+									}
+								
+								else if(chosenClass == 2)
+									{ 
+										System.out.println("You are changing the grade in " + s.getSecondClass());
+										
+										System.out.println("What would you like the new grade to be?");
+										
+										String chosenGrade2 = userInput.next(); 
+										
+										s.setSecondClassGrade(chosenGrade2);
+										
+										System.out.println("Okay, the grade has been changed");
+										
+									}
+								
+								else if(chosenClass == 3)
+									{ 
+										System.out.println("You are changing the grade in " + s.getThirdClass());
+										
+										System.out.println("What would you like the new grade to be?");
+										
+										String chosenGrade3 = userInput.next(); 
+										
+										s.setThirdClassGrade(chosenGrade3);
+										
+										System.out.println("Okay, the grade has been changed");
+										
+									}
+								
+								System.out.println("Here is the updated roster: "); 
+								Runner.printRoster();
+							
+								
+								
+								
+								
+												
+								
+							}
+					}
+				
+				
+				
+				
+				
+				
+				
+
+			}
+
 //Clancy: line 70
 		public static void switchClasses()
-		{
-			
-			System.out.println("Okay, here are all of the students: ");
-			
-			Runner.printRoster(); 
-			
-			//Scanner userInputString = new Scanner(System.in); 
-			
-			//String chosenStudentFirstName = userInputString.nextLine(); 
-			
-			
-			
-			
-		}
-		
-		
+			{
+
+				System.out.println("Okay, here are all of the students: ");
+
+				Runner.printRoster();
+
+				// Scanner userInputString = new Scanner(System.in);
+
+				// String chosenStudentFirstName = userInputString.nextLine();
+
+			}
+
 	}
